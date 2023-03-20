d71f1d6cc5ac3c65a7cf4fca2a96091a08f3a2e8
Mihkel Väli <mihkel@bobw.co>
2023-03-20 18:11:20 +0200

    matkade lisamine ja kustutamine

--- matkaklubi/admin/script.js
+++ matkaklubi/admin/script.js
@@ -1,6 +1,7 @@
 const API_URL = 'https://expressjs-postgres-production-f05d.up.railway.app';
 const sisu = document.querySelector('.matkad');
 const detail = document.querySelector('.detail');
+const uusMatk = document.querySelector('.uus-matk');
 
 async function getTreks() {
   const treks = await fetch(`${API_URL}/treks?columns=id,title`).then((response) => response.json());
@@ -45,10 +46,67 @@ async function getTrekDetails(id) {
       </select>
     </div>
     <button onclick="saveTrekDetails()">Salvesta</button>
+    <button onclick="deleteTrek()">Kustuta</button>
   `;
 
   const staatus =  document.querySelector('#staatus');
   staatus.value = trek.status;
+}
+
+function startCreatingNewTrek() {
+  detail.innerHTML = `
+    <div class="lahter">
+      <label for="pealkiri">Pealkiri</label>
+      <input id="pealkiri" />
+    </div>
+    <div class="lahter">
+      <label for="kirjeldus">Kirjeldus</label>
+      <input id="kirjeldus" />
+    </div>
+    <div class="lahter">
+      <label for="pildiUrl">Pildi URL</label>
+      <input id="pildiUrl" />
+    </div>
+    <div class="lahter">
+      <label for="kestvus">Kestvus</label>
+      <input id="kestvus" />
+    </div>
+    <div class="lahter">
+      <label for="staatus">Staatus</label>
+      <select id="staatus">
+        <option value="DRAFT">DRAFT</option>
+        <option value="REGISTRATION_OPEN">REGISTRATION_OPEN</option>
+        <option value="IN_PROGRESS">IN_PROGRESS</option>
+        <option value="COMPLETED">COMPLETED</option>
+      </select>
+    </div>
+    <button onclick="addNewTrek()">Loo uus matk</button>
+  `;
+}
+
+async function addNewTrek() {
+  const pealkiri = document.querySelector('#pealkiri').value;
+  const kirjeldus = document.querySelector('#kirjeldus').value;
+  const pildiUrl = document.querySelector('#pildiUrl').value;
+  const kestvus = document.querySelector('#kestvus').value;
+  const staatus =  document.querySelector('#staatus').value;
+
+  const response = await fetch(`${API_URL}/treks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: pealkiri,
+      description: kirjeldus,
+      image_url: pildiUrl,
+      duration: kestvus,
+      status: staatus
+    })
+  }).then((response) => response.json());
+
+  console.log(response);
+  getTreks();
 }
 
 async function saveTrekDetails() {
@@ -74,6 +132,21 @@ async function saveTrekDetails() {
   }).then((response) => response.json());
 
   console.log(response);
+  getTreks();
+}
+
+async function deleteTrek() {
+  if (!confirm('Oled kindel, et soovid matka kustutada?')) {
+    return;
+  }
+
+  const matkaId = document.querySelector('#matkaId').innerHTML;
+  const response = await fetch(`${API_URL}/treks/${matkaId}`, {
+    method: 'DELETE'
+  }).then((response) => response.json());
+
+  console.log(response);
+  detail.innerHTML = '';
   getTreks();
 }
 
