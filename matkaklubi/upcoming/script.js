c702cbf72a2216640ee224e829cddaa8a6c8622e
Mihkel Väli <mihkel@bobw.co>
2023-03-20 19:43:48 +0200

    matkadele registreerumine

--- matkaklubi/upcoming/script.js
+++ matkaklubi/upcoming/script.js
@@ -20,7 +20,69 @@ async function getTreks() {
       </div>
     `;
   }
-
 };
+
+async function signUp(trekId) {
+  const trek = await fetch(`${API_URL}/treks/${trekId}`).then(response => response.json());
+  sisu.innerHTML = `
+    <div class="registration">
+      <h3>Registreerumine matkale "${trek.title}"</h3>
+      <div class="lahter">
+        <label for="nimi">Osaleja nimi</label>
+        <input id="nimi" />
+      </div>
+      <div class="lahter">
+        <label for="vanus">Osaleja vanus</label>
+        <input id="vanus" />
+      </div>
+      <div class="lahter">
+        <label for="markus">Märkused/küsimused</label>
+        <textarea id="markus"></textarea>
+      </div>
+      <div class="vastutus">
+        <input id="noustub" type="checkbox" />
+        <label for="noustub">Nõustun liituma matkaga omal vastutusel</label>
+      </div>
+
+      <div class="veateade">Matka liitusemisega omal vastutusel peab nõustuma!</div>
+      <div class="onnestus">Olete matkale registreeritud!</div>
+
+      <div class="buttons-container">
+        <button onclick="saveRegistration(${trekId})">Registreeri</button>
+        <button class="inverted" onclick="getTreks()">Tagasi</button>
+      </div>
+    </div>
+  `;
+}
+
+async function saveRegistration(trekId) {
+  const nimi = document.querySelector('#nimi').value;
+  const vanus = document.querySelector('#vanus').value;
+  const markus = document.querySelector('#markus').value;
+  const noustub = document.querySelector('#noustub').checked;
+  const veateade = document.querySelector('.veateade');
+  const onnestus = document.querySelector('.onnestus');
+  
+  if (!noustub) {
+    onnestus.style.display = 'none';
+    veateade.style.display = 'block';
+    return;
+  }
+
+  await fetch(`${API_URL}/treks/${trekId}/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: nimi,
+      age: vanus,
+      note: markus
+    })
+  }).then(res => res.json());
+
+  veateade.style.display = 'none';
+  onnestus.style.display = 'block';
+}
 
 getTreks();
